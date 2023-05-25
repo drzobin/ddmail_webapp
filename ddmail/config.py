ce94caf3a7b203983f7a4bff9257dbeb19da18e8
drzobin <robin.larsson@protonmail.ch>
2023-05-25 12:06:47 +0200

    change hostname

--- ddmail/config.py
+++ ddmail/config.py
@@ -1,7 +1,7 @@
 class Prod():
     WTF_CSRF_SECRET_KEY = 'changeme'
     SECRET_KEY = 'changeme'
-    SERVER_NAME = '172.16.23.137:5000'
+    SERVER_NAME = 'www.ddmail.se'
     SQLALCHEMY_DATABASE_URI = 'mysql://mail_rw:password@localhost/mail'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
