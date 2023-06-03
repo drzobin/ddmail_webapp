4fafca7ba8aa09adcd0dbf85425427a3bae5faf3
drobin <me@drz.se>
2023-06-04 01:42:13 +0200

    Fix db model relationship problem

--- ddmail/ddmail/models.py
+++ ddmail/ddmail/models.py
@@ -13,65 +13,71 @@ class Account(db.Model):
     assets_in_sek = db.Column(db.Integer)
     is_enabled = db.Column(db.Boolean, unique=False, nullable=False)
     is_gratis = db.Column(db.Boolean, unique=False, nullable=False)
-    aliases = relationship("Alias")
-    emails = relationship("Email")
-    domains = relationship("Domain")
-    users = relationship("User")
+
+    aliases = relationship("Alias", back_populates="account")
+    emails = relationship("Email", back_populates="account")
+    domains = relationship("Domain", back_populates="account")
+    users = relationship("User", back_populates="account")
 
 # DB modul for aliases.
 class Alias(db.Model):
     __tablename__ = 'aliases'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    account_id = db.Column(db.Integer, ForeignKey('accounts.id'), nullable=False)
+    account_id = db.mapped_column(ForeignKey("accounts.id"), nullable=False)
     src_email = db.Column(db.String(200), unique=True, nullable=False)
-    src_domain_id = db.Column(db.Integer, ForeignKey('domains.id'), nullable=False)
-    dst_email_id = db.Column(db.Integer, ForeignKey('emails.id'), nullable=False)
-    account = relationship("Account")
-    email = relationship("Email")
-    domain = relationship("Domain")
+    src_domain_id = db.mapped_column(db.Integer, ForeignKey('domains.id'), nullable=False)
+    dst_email_id = db.mapped_column(db.Integer, ForeignKey('emails.id'), nullable=False)
+
+    account = relationship("Account", back_populates="aliases")
+    email = relationship("Email", back_populates="aliases")
+    domain = relationship("Domain", back_populates="aliases")
 
 # DB modul for emails.
 class Email(db.Model):
     __tablename__ = 'emails'
     id = db.Column(db.Integer, primary_key=True,nullable=False)
-    account_id = db.Column(db.Integer, ForeignKey('accounts.id'),nullable=False)
-    domain_id = db.Column(db.Integer, ForeignKey('domains.id'),nullable=False)
+    account_id = db.mapped_column(db.Integer, ForeignKey('accounts.id'),nullable=False)
+    domain_id = db.mapped_column(db.Integer, ForeignKey('domains.id'),nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
     password_hash = db.Column(db.String(2096), nullable=False)
+
     account = relationship("Account", back_populates="emails")
     domain = relationship("Domain", back_populates="emails")
-    aliases = relationship("Alias")
+    aliases = relationship("Alias", back_populates="email")
 
 # DB modul for domains.
 class Domain(db.Model):
     __tablename__ = 'domains'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    account_id = db.Column(db.Integer, ForeignKey('accounts.id'), nullable=False)
+    account_id = db.mapped_column(db.Integer, ForeignKey('accounts.id'), nullable=False)
     domain = db.Column(db.String(200), unique=True, nullable=False)
     is_global = db.Column(db.Boolean, unique=False, nullable=False,default=False)
+
     account = relationship("Account", back_populates="domains")
-    emails = relationship("Email")
-    aliases = relationship("Alias")
+    emails = relationship("Email", back_populates="domain")
+    aliases = relationship("Alias", back_populates="domain")
 
 # DB modul for users.
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    account_id = db.Column(db.Integer, ForeignKey('accounts.id'), nullable=False)
+    account_id = db.mapped_column(db.Integer, ForeignKey('accounts.id'), nullable=False)
     user = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(200), unique=True, nullable=False)
     password_key_hash = db.Column(db.String(200), unique=True, nullable=False)
+
     account = relationship("Account", back_populates="users")
-    authenticated = relationship("Authenticated")
+    authenticated = relationship("Authenticated", back_populates="user")
 
 # DB model for authenticated.
 class Authenticated(db.Model):
     __tablename__ = 'authenticateds'
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     cookie = db.Column(db.String(12), unique=True, nullable=False)
-    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
+    user_id = db.mapped_column(db.Integer, ForeignKey('users.id'), nullable=False)
     valid_to = db.Column(db.DateTime, nullable=False)
-    user = relationship("User")
+
+    user = relationship("User", back_populates="authenticated")
 
     def __init__(self,cookie,user_id,valid_to):
         self.cookie = cookie
