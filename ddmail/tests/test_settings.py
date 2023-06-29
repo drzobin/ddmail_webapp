3efc63de9def9d40db3043ae8d20cf54907f67f9
drobin <robin.larsson@protonmail.ch>
2023-06-30 00:13:41 +0200

    Add test for /settings/show_domains with disabled account

--- ddmail/tests/test_settings.py
+++ ddmail/tests/test_settings.py
@@ -611,6 +611,47 @@ def test_settings_disabled_account_remove_alias(client,app):
 
         db.session.query(Account).filter(Account.account == register_data["account"]).delete()
 
+def test_settings_disabled_account_show_domains(client,app):
+    # Get the csrf token for /register
+    response_register_get = client.get("/register")
+    csrf_token_register = get_csrf_token(response_register_get.data)
+
+    # Register account and user
+    response_register_post = client.post("/register", data={'csrf_token':csrf_token_register})
+    register_data = get_register_data(response_register_post.data)
+
+    # Get csrf_token from /login
+    response_login_get = client.get("/login")
+    csrf_token_login = get_csrf_token(response_login_get.data)
+
+    # Test POST /login with newly registred account and user.
+    assert client.post("/login", buffered=True, content_type='multipart/form-data', data={'user':register_data["username"], 'password':register_data["password"], 'key':(BytesIO(bytes(register_data["key"], 'utf-8')), 'data.key') ,'csrf_token':csrf_token_login}).status_code == 302
+
+    # Test POST /login with newly registred account and user, check that account and username is correct and that account is disabled.
+    response_login_post = client.post("/login", buffered=True, content_type='multipart/form-data', data={'user':register_data["username"], 'password':register_data["password"], 'key':(BytesIO(bytes(register_data["key"], 'utf-8')), 'data.key') ,'csrf_token':csrf_token_login},follow_redirects = True)
+    assert b"Logged in on account: " + bytes(register_data["account"], 'utf-8') in response_login_post.data
+    assert b"Logged in as user: " + bytes(register_data["username"], 'utf-8') in response_login_post.data
+    assert b"Is account enabled: No" in response_login_post.data
+
+    # Test GET /settings/show_domains
+    assert client.get("/settings/show_domains").status_code == 200
+    response_settings_show_domains_get = client.get("/settings/show_domains")
+    assert b"Logged in on account: " + bytes(register_data["account"], 'utf-8') in response_settings_show_domains_get.data
+    assert b"Logged in as user: " + bytes(register_data["username"], 'utf-8') in response_settings_show_domains_get.data
+    assert b"Is account enabled: No" in response_settings_show_domains_get.data
+    assert b"Failed to show domains beacuse this account is disabled. In order to enable the account you need to pay, see payments option in menu." in response_settings_show_domains_get.data
+
+    # Remove authenticated, user and account that was used in testcase.
+    with app.app_context():
+        user_from_db = db.session.query(User).filter(User.user == register_data["username"]).first()
+        db.session.query(Authenticated).filter(Authenticated.user_id == user_from_db.id).delete()
+        db.session.commit()
+
+        db.session.query(User).filter(User.user == register_data["username"]).delete()
+        db.session.commit()
+
+        db.session.query(Account).filter(Account.account == register_data["account"]).delete()
+
 def test_settings_disabled_account_add_domain(client,app):
     # Get the csrf token for /register
     response_register_get = client.get("/register")
