04f6d8321716890f7f22f34b718f0a2ef5d3ce77
drobin <robin.larsson@protonmail.ch>
2023-07-17 09:03:23 +0200

    Remove test that is not needed

--- ddmail/tests/test_settings.py
+++ ddmail/tests/test_settings.py
@@ -398,12 +398,6 @@ def test_settings_disabled_account_add_user_to_account(client,app):
     # Test POST /login with newly registred account and user.
     assert client.post("/login", buffered=True, content_type='multipart/form-data', data={'user':register_data["username"], 'password':register_data["password"], 'key':(BytesIO(bytes(register_data["key"], 'utf-8')), 'data.key') ,'csrf_token':csrf_token_login}).status_code == 302
 
-    # Test POST /login with newly registred account and user, check that account and username is correct and that account is disabled.
-    response_login_post = client.post("/login", buffered=True, content_type='multipart/form-data', data={'user':register_data["username"], 'password':register_data["password"], 'key':(BytesIO(bytes(register_data["key"], 'utf-8')), 'data.key') ,'csrf_token':csrf_token_login},follow_redirects = True)
-    assert b"Logged in on account: " + bytes(register_data["account"], 'utf-8') in response_login_post.data
-    assert b"Logged in as user: " + bytes(register_data["username"], 'utf-8') in response_login_post.data
-    assert b"Is account enabled: No" in response_login_post.data
-
     # Test GET /settings/add_user_to_account.
     assert client.get("/settings/add_user_to_account").status_code == 200
     response_settings_add_user_to_account_get = client.get("/settings/add_user_to_account")
