d09af60a68c3a35d17bb41a22a6ce1f7381e6910
drobin <robin.larsson@protonmail.ch>
2023-08-19 17:17:29 +0200

    Add test for enabled account /settings/show_alias

--- ddmail/tests/test_settings.py
+++ ddmail/tests/test_settings.py
@@ -1365,6 +1365,35 @@ def test_settings_disabled_account_show_alias(client,app):
 
         db.session.query(Account).filter(Account.account == register_data["account"]).delete()
 
+def test_settings_enabled_account_show_alias(client,app):
+    # Get the csrf token for /register
+    response_register_get = client.get("/register")
+    csrf_token_register = get_csrf_token(response_register_get.data)
+
+    # Register account and user
+    response_register_post = client.post("/register", data={'csrf_token':csrf_token_register})
+    register_data = get_register_data(response_register_post.data)
+    
+    # Enable account.
+    with app.app_context():
+        account = db.session.query(Account).filter(Account.account == register_data["account"]).first()
+        account.is_enabled = True        
+        db.session.commit()
+
+    # Get csrf_token from /login
+    response_login_get = client.get("/login")
+    csrf_token_login = get_csrf_token(response_login_get.data)
+
+    # Test POST /login with newly registred account and user.
+    assert client.post("/login", buffered=True, content_type='multipart/form-data', data={'user':register_data["username"], 'password':register_data["password"], 'key':(BytesIO(bytes(register_data["key"], 'utf-8')), 'data.key') ,'csrf_token':csrf_token_login}).status_code == 302
+
+    # Test GET /settings/show_alias
+    assert client.get("/settings/show_alias").status_code == 200
+    response_settings_show_alias_get = client.get("/settings/show_alias")
+    assert b"Logged in on account: " + bytes(register_data["account"], 'utf-8') in response_settings_show_alias_get.data
+    assert b"Logged in as user: " + bytes(register_data["username"], 'utf-8') in response_settings_show_alias_get.data
+    assert b"Is account enabled: Yes" in response_settings_show_alias_get.data
+
 def test_settings_disabled_account_add_alias(client,app):
     # Get the csrf token for /register
     response_register_get = client.get("/register")
