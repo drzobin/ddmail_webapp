28d9b4137931abc4caa680e40d55c21ca0480c1c
drobin <robin.larsson@protonmail.ch>
2023-08-20 23:10:54 +0200

    Use different db users for prod/test/dev

--- ddmail/config.py
+++ ddmail/config.py
@@ -7,11 +7,11 @@ class Prod():
 class Test():
     WTF_CSRF_SECRET_KEY = 'password'
     SECRET_KEY = 'password'
-    SQLALCHEMY_DATABASE_URI = 'mysql://mail_rw:password@localhost/mail_test'
+    SQLALCHEMY_DATABASE_URI = 'mysql://mail_test_rw:password@localhost/mail_test'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
 class Dev():
     WTF_CSRF_SECRET_KEY = 'password'
     SECRET_KEY = 'password'
-    SQLALCHEMY_DATABASE_URI = 'mysql://mail_rw:password@localhost/mail_dev'
+    SQLALCHEMY_DATABASE_URI = 'mysql://mail_dev_rw:password@localhost/mail_dev'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
