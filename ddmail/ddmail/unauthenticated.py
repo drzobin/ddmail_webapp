73b1f4a82a6864a15f38a33b184e8ec70b7c0875
drobin <robin.larsson@protonmail.ch>
2023-09-22 11:16:41 +0200

    Add /help

--- ddmail/ddmail/unauthenticated.py
+++ ddmail/ddmail/unauthenticated.py
@@ -13,6 +13,16 @@ def main():
 
     return render_template('main.html', current_user = current_user)
 
+@bp.route("/help")
+def help():
+    # Check if user is athenticated.
+    if "secret" in session:
+        current_user = is_athenticated(session["secret"])
+    else:
+        current_user = None
+
+    return render_template('help.html',current_user=current_user)
+
 @bp.route("/about")
 def about():
     # Check if user is athenticated.
