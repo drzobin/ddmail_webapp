252777694a69699ffd3644a33b7a605d1b94239b
drobin <robin.larsson@protonmail.ch>
2023-09-24 21:39:34 +0200

    Add support for ddmail_email_remover

--- ddmail/config.py
+++ ddmail/config.py
@@ -2,16 +2,22 @@ class Prod():
     WTF_CSRF_SECRET_KEY = 'password'
     SECRET_KEY = 'password'
     SQLALCHEMY_DATABASE_URI = 'mysql://mail_rw:password@localhost/mail'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    EMAIL_REMOVER_URL = '127.0.0.1:8001'
+    EMAIL_REMOVER_PASSWORD = 'password'
 
 class Test():
     WTF_CSRF_SECRET_KEY = 'password'
     SECRET_KEY = 'password'
     SQLALCHEMY_DATABASE_URI = 'mysql://mail_test_rw:password@localhost/mail_test'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    EMAIL_REMOVER_URL = '127.0.0.1:8001'
+    EMAIL_REMOVER_PASSWORD = 'password'
 
 class Dev():
     WTF_CSRF_SECRET_KEY = 'password'
     SECRET_KEY = 'password'
     SQLALCHEMY_DATABASE_URI = 'mysql://mail_dev_rw:password@localhost/mail_dev'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    EMAIL_REMOVER_URL = '127.0.0.1:8001'
+    EMAIL_REMOVER_PASSWORD = 'password'
