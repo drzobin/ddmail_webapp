4020d270edf4b8f695c7ed5ebaa9339412aa3c71
drz <me@drz.se>
2024-04-20 09:35:44 +0200

    Add validators for openpgp public key and its fingerprint

--- ddmail/ddmail/validators.py
+++ ddmail/ddmail/validators.py
@@ -126,3 +126,43 @@ def is_dmarc_valid(domain,dmarc_record):
                 return False
     except:
         return False
+
+# Validate openpgp public key. Only allow the following chars: A-Z, a-z, 0-9 and +/=
+def is_openpgp_public_key_allowed(public_key):
+    if public_key == None:
+        return False
+
+    # Check start and end of string.
+    if public_key.startswith("-----BEGIN PGP PUBLIC KEY BLOCK-----") != True:
+        return False
+    if public_key.endswith("-----END PGP PUBLIC KEY BLOCK-----") != True:
+        return False
+
+    public_key = public_key.replace("-----BEGIN PGP PUBLIC KEY BLOCK-----","", 1)
+    public_key = public_key.replace("-----END PGP PUBLIC KEY BLOCK-----","", 1)
+
+    # Only allow A-Z ,a-z, 0-9 and +/=
+    pattern = re.compile(r"[a-zA-Z0-9\+\/\=\s]")
+    for char in public_key:
+        if not re.match(pattern, char):
+            return False
+
+    return True
+
+# Validate openpgp public key fingerprint string. Only allow the following chars: A-Z, 0-9
+def is_openpgp_key_fingerprint_allowed(fingerprint):
+    if fingerprint == None:
+        return False
+
+    # Fingerprint string should be 40 char.
+    allowed_len = 40
+    if len(fingerprint) != allowed_len:
+        return False
+
+    # Only allow A-Z, 0-9
+    pattern = re.compile(r"[A-Z0-9]")
+    for char in fingerprint:
+        if not re.match(pattern, char):
+            return False
+
+    return True
