d9b9cedd26e730fecbd40c93ac6ef1a07b7a44a3
drzobin <me@drz.se>
2025-04-03 19:25:35 +0200

    Start using ddmail_validators from pypi and refactor to make logging better

--- src/ddmail_webapp/auth.py
+++ src/ddmail_webapp/auth.py
@@ -6,7 +6,7 @@ from flask import Blueprint, request, render_template, session, redirect, url_fo
 from argon2 import PasswordHasher
 from argon2.exceptions import VerifyMismatchError
 from ddmail_webapp.models import db, Account, User, Authenticated
-from ddmail_webapp.validators import is_username_allowed, is_password_allowed
+import ddmail_validators.validators as validators
 
 bp = Blueprint("auth", __name__, url_prefix="/")
 
@@ -31,7 +31,7 @@ def generate_password(length):
 # Check if a user is authenticated, if the user is authenticated the user id will be returned else None.
 def is_athenticated(cookie):
     # Validate the cookie
-    if is_password_allowed(cookie) != True:
+    if validators.is_cookie_allowed(cookie) != True:
         return None
 
     # Try to find the cookie in the db.
@@ -105,7 +105,6 @@ def login():
         cleartext_password_from_form = request.form["password"].strip()
         file = request.files['key']
         cleartext_password_key_from_form = file.read().strip().decode("utf-8")
-        
 
         # Check that form has data.
         if not user_from_form or not cleartext_password_from_form or not cleartext_password_key_from_form:
@@ -113,50 +112,71 @@ def login():
             current_app.logger.warning("failed login, data is missing")
             return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
 
-        # Validate the form data.
-        if is_username_allowed(user_from_form) != True or is_password_allowed(cleartext_password_from_form) != True or is_password_allowed(cleartext_password_key_from_form) != True:
+        # Validate the form data username.
+        if validators.is_username_allowed(user_from_form) != True:
             # Login failed.
-            current_app.logger.warning("failed login, validation failed")
+            current_app.logger.warning("failed login, username validation failed")
             return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
 
-        # Get the user data from db.
-        user_from_db = db.session.query(User).filter(User.user == user_from_form).first()
+        # Validate the form data password.
+        if validators.is_password_allowed(cleartext_password_from_form) != True:
+            # Login failed.
+            current_app.logger.warning("failed login, password validation failed")
+            return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
 
+        # Validate the form data password key.
+        if validators.is_password_key_allowed(cleartext_password_key_from_form) != True:
+            # Login failed.
+            current_app.logger.warning("failed login, password key validation failed")
+            return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
+
+        # Get the user data from db and check that user exist.
+        user_from_db = db.session.query(User).filter(User.user == user_from_form).first()
         if not user_from_db:
             # Login failed.
-            current_app.logger.warning("failed login for user " + user_from_form + " do not exsist in db")
+            current_app.logger.warning("failed login, user " + user_from_form + " do not exsist in db")
             return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
 
-        # Check password hash and password key hash.
+        # Check password hash.
         try:
-            if ph.verify(user_from_db.password_hash, cleartext_password_from_form) == True and ph.verify(user_from_db.password_key_hash, cleartext_password_key_from_form) == True:
-                # Generate a secret random cookie.
-                cookie = generate_password(128)
-
-                # Sign the cookie and store it in the browser.
-                session["secret"] = cookie
-
-                # Store the cookie in the db together with expire time.
-                authenticated = Authenticated(cookie,user_from_db.id,datetime.datetime.now() + datetime.timedelta(hours=3))
-                db.session.add(authenticated)
-                db.session.commit()
-
-                current_app.logger.info("successful login for user " + user_from_db.user + " belonging to account " + user_from_db.account.account)
-                return redirect('/settings')
-                #return render_template('settings.html',current_user=current_user)
-            else:
+            if ph.verify(user_from_db.password_hash, cleartext_password_from_form) != True:
                 # Login failed.
-                current_app.logger.warning("failed login for user " + user_from_db.user + " belonging to account " + user_from_db.account.account + " wrong password and/or key")
+                current_app.logger.warning("failed login, user " + user_from_db.user + " belonging to account " + user_from_db.account.account + " wrong password")
                 return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
         except VerifyMismatchError:
             # Login failed.
-            current_app.logger.warning("failed login for user " + user_from_db.user + " belonging to account " + user_from_db.account.account + " wrong password and/or key")
+            current_app.logger.warning("failed login, user " + user_from_db.user + " belonging to account " + user_from_db.account.account + " wrong password")
             return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
 
+        # Check password key hash.
+        try:
+            if ph.verify(user_from_db.password_key_hash, cleartext_password_key_from_form) != True:
+                # Login failed.
+                current_app.logger.warning("failed login, user " + user_from_db.user + " belonging to account " + user_from_db.account.account + " wrong password key")
+                return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
+        except VerifyMismatchError:
+            # Login failed.
+            current_app.logger.warning("failed login, user " + user_from_db.user + " belonging to account " + user_from_db.account.account + " wrong password key")
+            return render_template('message.html',headline="Login error",message="Failed to login, wrong username and/or password and/or key.",current_user=current_user)
+
+        # Login succeeded
+        # Generate a secret random cookie.
+        cookie = generate_password(128)
+
+        # Sign the cookie and store it in the browser.
+        session["secret"] = cookie
+
+        # Store the cookie in the db together with expire time.
+        authenticated = Authenticated(cookie,user_from_db.id,datetime.datetime.now() + datetime.timedelta(hours=3))
+        db.session.add(authenticated)
+        db.session.commit()
+
+        current_app.logger.info("successful login for user " + user_from_db.user + " belonging to account " + user_from_db.account.account)
+        return redirect('/settings')
 
 @bp.route("/logout")
 def logout():
-    # Check if user is athenticated.
+    # Check if user is authenticated.
     if "secret" in session:
         current_user = is_athenticated(session["secret"])
         current_app.logger.debug("secret is in session")
