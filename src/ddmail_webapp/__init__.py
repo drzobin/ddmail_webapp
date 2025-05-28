a8d5f51c32215eb7906a297c8ee4953aa3c8b0ce
drzobin <me@drz.se>
2025-05-28 20:30:19 +0200

    Add support for syslog logging

--- src/ddmail_webapp/__init__.py
+++ src/ddmail_webapp/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import logging
+import logging.handlers
 import toml
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
@@ -10,7 +11,7 @@ from logging import FileHandler
 def create_app(config_file=None, test_config=None):
     """Create and configure an instance of the Flask application ddmail."""
     # Configure logging.
-    log_format = '[%(asctime)s] %(levelname)s in %(module)s %(funcName)s %(lineno)s: %(message)s'
+    log_format = '[%(asctime)s] ddmail_webapp %(levelname)s in %(module)s %(funcName)s %(lineno)s: %(message)s'
     dictConfig({
         'version': 1,
         'formatters': {'default': {
@@ -44,137 +45,56 @@ def create_app(config_file=None, test_config=None):
 
     # Set app configurations from toml config file.
     mode = os.environ.get('MODE')
-    print("Running in MODE: " + mode)
+    print("Running in MODE: " + str(mode))
 
     # Apply configuration for the specific MODE.
-    if mode == "PRODUCTION":
-        app.config["SECRET_KEY"] = toml_config["PRODUCTION"]["SECRET_KEY"]
-        app.config['SQLALCHEMY_DATABASE_URI'] = toml_config["PRODUCTION"]['SQLALCHEMY_DATABASE_URI']
-        app.config["WTF_CSRF_SECRET_KEY"] = toml_config["PRODUCTION"]["WTF_CSRF_SECRET_KEY"]
+    if mode == "PRODUCTION" or mode == "TESTING" or mode == "DEVELOPMENT":
+        app.config["SECRET_KEY"] = toml_config[mode]["SECRET_KEY"]
+        app.config['SQLALCHEMY_DATABASE_URI'] = toml_config[mode]['SQLALCHEMY_DATABASE_URI']
+        app.config["WTF_CSRF_SECRET_KEY"] = toml_config[mode]["WTF_CSRF_SECRET_KEY"]
 
-        # Configure services that ddmail_webapp depend on. 
-        app.config["EMAIL_REMOVER_URL"] = toml_config["PRODUCTION"]["EMAIL_REMOVER_URL"]
-        app.config["EMAIL_REMOVER_PASSWORD"] = toml_config["PRODUCTION"]["EMAIL_REMOVER_PASSWORD"]
-        app.config["DMCP_KEYHANDLER_URL"] = toml_config["PRODUCTION"]["DMCP_KEYHANDLER_URL"]
-        app.config["DMCP_KEYHANDLER_PASSWORD"] = toml_config["PRODUCTION"]["DMCP_KEYHANDLER_PASSWORD"]
-        app.config["OPENPGP_KEYHANDLER_URL"] = toml_config["PRODUCTION"]["OPENPGP_KEYHANDLER_URL"]
-        app.config["OPENPGP_KEYHANDLER_PASSWORD"] = toml_config["PRODUCTION"]["OPENPGP_KEYHANDLER_PASSWORD"]
-
-        # Configure payment information.
-        app.config["PAYMENT_BANKGIRO"] = toml_config["PRODUCTION"]["PAYMENT_BANKGIRO"]
-        
-        # Configure dns record checked when adding account/own domains.
-        app.config["MX_RECORD_HOST"] = toml_config["PRODUCTION"]["MX_RECORD_HOST"]
-        app.config["MX_RECORD_PRIORITY"] = toml_config["PRODUCTION"]["MX_RECORD_PRIORITY"]
-        app.config["SPF_RECORD"] = toml_config["PRODUCTION"]["SPF_RECORD"]
-        app.config["DKIM_RECORD"] = toml_config["PRODUCTION"]["DKIM_RECORD"]
-        app.config["DMARC_RECORD"] = toml_config["PRODUCTION"]["DMARC_RECORD"]
-
-        # Configure tor and i2p addres.
-        app.config["TOR_ADDRESS"] = toml_config["PRODUCTION"]["TOR_ADDRESS"]
-        app.config["I2P_ADDRESS"] = toml_config["PRODUCTION"]["I2P_ADDRESS"]
-
-        # Configure logfile.
-        file_handler = FileHandler(filename=toml_config["PRODUCTION"]["LOGFILE"])
-        file_handler.setFormatter(logging.Formatter(log_format))
-        app.logger.addHandler(file_handler)
-        
-        # Configure loglevel.
-        if toml_config["PRODUCTION"]["LOGLEVEL"] == "ERROR":
-            app.logger.setLevel(logging.ERROR)
-        elif toml_config["PRODUCTION"]["LOGLEVEL"] == "WARNING":
-            app.logger.setLevel(logging.WARNING)
-        elif toml_config["PRODUCTION"]["LOGLEVEL"] == "INFO":
-            app.logger.setLevel(logging.INFO)
-        elif toml_config["PRODUCTION"]["LOGLEVEL"] == "DEBUG":
-            app.logger.setLevel(logging.DEBUG)
-        else:
-            print("Error: you need to set LOGLEVEL to ERROR/WARNING/INFO/DEBUG")
-            sys.exit(1)
-    elif mode == "TESTING":
-        app.config["SECRET_KEY"] = toml_config["TESTING"]["SECRET_KEY"]
-        app.config['SQLALCHEMY_DATABASE_URI'] = toml_config["TESTING"]['SQLALCHEMY_DATABASE_URI']
-        app.config["WTF_CSRF_SECRET_KEY"] = toml_config["TESTING"]["WTF_CSRF_SECRET_KEY"]
-
-        # Configure services that ddmail_webapp depend on. 
-        app.config["EMAIL_REMOVER_URL"] = toml_config["TESTING"]["EMAIL_REMOVER_URL"]
-        app.config["EMAIL_REMOVER_PASSWORD"] = toml_config["TESTING"]["EMAIL_REMOVER_PASSWORD"]
-        app.config["DMCP_KEYHANDLER_URL"] = toml_config["TESTING"]["DMCP_KEYHANDLER_URL"]
-        app.config["DMCP_KEYHANDLER_PASSWORD"] = toml_config["TESTING"]["DMCP_KEYHANDLER_PASSWORD"]
-        app.config["OPENPGP_KEYHANDLER_URL"] = toml_config["TESTING"]["OPENPGP_KEYHANDLER_URL"]
-        app.config["OPENPGP_KEYHANDLER_PASSWORD"] = toml_config["TESTING"]["OPENPGP_KEYHANDLER_PASSWORD"]
+        # Configure services that ddmail_webapp depend on.
+        app.config["EMAIL_REMOVER_URL"] = toml_config[mode]["EMAIL_REMOVER_URL"]
+        app.config["EMAIL_REMOVER_PASSWORD"] = toml_config[mode]["EMAIL_REMOVER_PASSWORD"]
+        app.config["DMCP_KEYHANDLER_URL"] = toml_config[mode]["DMCP_KEYHANDLER_URL"]
+        app.config["DMCP_KEYHANDLER_PASSWORD"] = toml_config[mode]["DMCP_KEYHANDLER_PASSWORD"]
+        app.config["OPENPGP_KEYHANDLER_URL"] = toml_config[mode]["OPENPGP_KEYHANDLER_URL"]
+        app.config["OPENPGP_KEYHANDLER_PASSWORD"] = toml_config[mode]["OPENPGP_KEYHANDLER_PASSWORD"]
 
         # Configure payment information.
-        app.config["PAYMENT_BANKGIRO"] = toml_config["TESTING"]["PAYMENT_BANKGIRO"]
-        
+        app.config["PAYMENT_BANKGIRO"] = toml_config[mode]["PAYMENT_BANKGIRO"]
+
         # Configure dns record checked when adding account/own domains.
-        app.config["MX_RECORD_HOST"] = toml_config["TESTING"]["MX_RECORD_HOST"]
-        app.config["MX_RECORD_PRIORITY"] = toml_config["TESTING"]["MX_RECORD_PRIORITY"]
-        app.config["SPF_RECORD"] = toml_config["TESTING"]["SPF_RECORD"]
-        app.config["DKIM_RECORD"] = toml_config["TESTING"]["DKIM_RECORD"]
-        app.config["DMARC_RECORD"] = toml_config["TESTING"]["DMARC_RECORD"]
+        app.config["MX_RECORD_HOST"] = toml_config[mode]["MX_RECORD_HOST"]
+        app.config["MX_RECORD_PRIORITY"] = toml_config[mode]["MX_RECORD_PRIORITY"]
+        app.config["SPF_RECORD"] = toml_config[mode]["SPF_RECORD"]
+        app.config["DKIM_RECORD"] = toml_config[mode]["DKIM_RECORD"]
+        app.config["DMARC_RECORD"] = toml_config[mode]["DMARC_RECORD"]
 
         # Configure tor and i2p addres.
-        app.config["TOR_ADDRESS"] = toml_config["TESTING"]["TOR_ADDRESS"]
-        app.config["I2P_ADDRESS"] = toml_config["TESTING"]["I2P_ADDRESS"]
+        app.config["TOR_ADDRESS"] = toml_config[mode]["TOR_ADDRESS"]
+        app.config["I2P_ADDRESS"] = toml_config[mode]["I2P_ADDRESS"]
 
-        # Configure logfile.
-        file_handler = FileHandler(filename=toml_config["TESTING"]["LOGFILE"])
-        file_handler.setFormatter(logging.Formatter(log_format))
-        app.logger.addHandler(file_handler)
+        # Configure logging to file.
+        if toml_config[mode]["LOGGING"]["LOG_TO_FILE"] == True:
+            file_handler = FileHandler(filename=toml_config[mode]["LOGGING"]["LOGFILE"])
+            file_handler.setFormatter(logging.Formatter(log_format))
+            app.logger.addHandler(file_handler)
 
-        # Configure loglevel.
-        if toml_config["TESTING"]["LOGLEVEL"] == "ERROR":
-            app.logger.setLevel(logging.ERROR)
-        elif toml_config["TESTING"]["LOGLEVEL"] == "WARNING":
-            app.logger.setLevel(logging.WARNING)
-        elif toml_config["TESTING"]["LOGLEVEL"] == "INFO":
-            app.logger.setLevel(logging.INFO)
-        elif toml_config["TESTING"]["LOGLEVEL"] == "DEBUG":
-            app.logger.setLevel(logging.DEBUG)
-        else:
-            print("Error: you need to set LOGLEVEL to ERROR/WARNING/INFO/DEBUG")
-            sys.exit(1)
-    elif mode == "DEVELOPMENT":
-        app.config["SECRET_KEY"] = toml_config["DEVELOPMENT"]["SECRET_KEY"]
-        app.config['SQLALCHEMY_DATABASE_URI'] = toml_config["DEVELOPMENT"]['SQLALCHEMY_DATABASE_URI']
-        app.config["WTF_CSRF_SECRET_KEY"] = toml_config["DEVELOPMENT"]["WTF_CSRF_SECRET_KEY"]
-
-        # Configure services that ddmail_webapp depend on. 
-        app.config["EMAIL_REMOVER_URL"] = toml_config["DEVELOPMENT"]["EMAIL_REMOVER_URL"]
-        app.config["EMAIL_REMOVER_PASSWORD"] = toml_config["DEVELOPMENT"]["EMAIL_REMOVER_PASSWORD"]
-        app.config["DMCP_KEYHANDLER_URL"] = toml_config["DEVELOPMENT"]["DMCP_KEYHANDLER_URL"]
-        app.config["DMCP_KEYHANDLER_PASSWORD"] = toml_config["DEVELOPMENT"]["DMCP_KEYHANDLER_PASSWORD"]
-        app.config["OPENPGP_KEYHANDLER_URL"] = toml_config["DEVELOPMENT"]["OPENPGP_KEYHANDLER_URL"]
-        app.config["OPENPGP_KEYHANDLER_PASSWORD"] = toml_config["DEVELOPMENT"]["OPENPGP_KEYHANDLER_PASSWORD"]
-
-        # Configure payment information.
-        app.config["PAYMENT_BANKGIRO"] = toml_config["DEVELOPMENT"]["PAYMENT_BANKGIRO"]
-        
-        # Configure dns record checked when adding account/own domains.
-        app.config["MX_RECORD_HOST"] = toml_config["DEVELOPMENT"]["MX_RECORD_HOST"]
-        app.config["MX_RECORD_PRIORITY"] = toml_config["DEVELOPMENT"]["MX_RECORD_PRIORITY"]
-        app.config["SPF_RECORD"] = toml_config["DEVELOPMENT"]["SPF_RECORD"]
-        app.config["DKIM_RECORD"] = toml_config["DEVELOPMENT"]["DKIM_RECORD"]
-        app.config["DMARC_RECORD"] = toml_config["DEVELOPMENT"]["DMARC_RECORD"]
-        
-        # Configure tor and i2p addres.
-        app.config["TOR_ADDRESS"] = toml_config["DEVELOPMENT"]["TOR_ADDRESS"]
-        app.config["I2P_ADDRESS"] = toml_config["DEVELOPMENT"]["I2P_ADDRESS"]
-
-        # Configure logfile.
-        file_handler = FileHandler(filename=toml_config["DEVELOPMENT"]["LOGFILE"])
-        file_handler.setFormatter(logging.Formatter(log_format))
-        app.logger.addHandler(file_handler)
+        # Configure logging to syslog.
+        if toml_config[mode]["LOGGING"]["LOG_TO_SYSLOG"] == True:
+            syslog_handler = logging.handlers.SysLogHandler(address = toml_config[mode]["LOGGING"]["SYSLOG_SERVER"])
+            syslog_handler.setFormatter(logging.Formatter(log_format))
+            app.logger.addHandler(syslog_handler)
 
         # Configure loglevel.
-        if toml_config["DEVELOPMENT"]["LOGLEVEL"] == "ERROR":
+        if toml_config[mode]["LOGGING"]["LOGLEVEL"] == "ERROR":
             app.logger.setLevel(logging.ERROR)
-        elif toml_config["DEVELOPMENT"]["LOGLEVEL"] == "WARNING":
+        elif toml_config[mode]["LOGGING"]["LOGLEVEL"] == "WARNING":
             app.logger.setLevel(logging.WARNING)
-        elif toml_config["DEVELOPMENT"]["LOGLEVEL"] == "INFO":
+        elif toml_config[mode]["LOGGING"]["LOGLEVEL"] == "INFO":
             app.logger.setLevel(logging.INFO)
-        elif toml_config["DEVELOPMENT"]["LOGLEVEL"] == "DEBUG":
+        elif toml_config[mode]["LOGGING"]["LOGLEVEL"] == "DEBUG":
             app.logger.setLevel(logging.DEBUG)
         else:
             print("Error: you need to set LOGLEVEL to ERROR/WARNING/INFO/DEBUG")
@@ -182,7 +102,7 @@ def create_app(config_file=None, test_config=None):
     else:
         print("Error: you need to set env variabel MODE to PRODUCTION/TESTING/DEVELOPMENT")
         sys.exit(1)
-    
+
     app.secret_key = app.config["SECRET_KEY"]
     app.WTF_CSRF_SECRET_KEY = app.config["WTF_CSRF_SECRET_KEY"]
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -210,4 +130,4 @@ def create_app(config_file=None, test_config=None):
     app.register_blueprint(unauthenticated.bp)
     app.register_blueprint(well_known.bp)
 
-    return app 
+    return app
