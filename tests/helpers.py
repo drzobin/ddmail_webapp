e4876b4ba8d96fe8d6eb967205e5d1c994f24d45
drzobin <me@drz.se>
2025-09-06 19:59:43 +0200

    Fix problem when regex cant find any csrf value

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,8 +1,11 @@
 import re
 
 def get_csrf_token(data):
-    m = re.search(b'<input type="hidden" name="csrf_token" value="(.*)"/>', data)
-    csrf_token = m.group(1).decode("utf-8")
+    m = re.search(b'<input type="hidden" name="csrf_token" value="(.*)"', data)
+    if m:
+        csrf_token = m.group(1).decode("utf-8")
+    else:
+        csrf_token = None
 
     return csrf_token
 
@@ -12,19 +15,17 @@ def get_register_data(data):
     # Get account
     m = re.search(b'<p>Account: (.*)</p>', data)
     register_data["account"] = m.group(1).decode("utf-8")
-    
+
     # Get username
     m = re.search(b'<p>Username: (.*)</p>', data)
     register_data["username"] = m.group(1).decode("utf-8")
-   
+
    #Get password
     m = re.search(b'<p>Password: (.*)</p>', data)
     register_data["password"] = m.group(1).decode("utf-8")
-    
+
     #Get key
     m = re.search(b'<p>Key file content: (.*)</p>', data)
     register_data["key"] = m.group(1).decode("utf-8")
 
     return register_data
-
-
